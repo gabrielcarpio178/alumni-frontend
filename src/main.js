5a3c0ba2a2de6042a2b08513eb9267cea758faa6
Gabriel Carpio <gabrielcarpio178@gmail.com>
2024-11-17 02:56:16 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -139,7 +139,7 @@ router.beforeEach(async (to, from, next) => {
 const globalVariable = {
     data(){
         return {
-            PORT: 'https://alumniserver-v3romq4n.b4a.run',
+            PORT: 'https://alumni-server-seven.vercel.app',
             isLogin: localStorage.getItem('token')!==null
         }
     }
